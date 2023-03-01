70a7b6bf2cba218344970f19a814d5e0fe697cb1
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:11:51 +0000

    creating resource group

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 RESOURCE_SUFFIX=$(openssl rand -base64 20 | tr -dc 'a-z0-9' | head -c 6)
 LOCATION="westus"
 SUBSCRIPTION_ID=$(az account show --query "id" --output "tsv")
+
+az group create --name "arkansas-${RESOURCE_SUFFIX}-rg" --location "${LOCATION}"
