c502dc9fab6a3047ac994bee3205391528c6e66c
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:12:50 +0000

    creating application insights component

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,5 @@ az group create --name "arkansas-${RESOURCE_SUFFIX}-rg" --location "${LOCATION}"
 az appservice plan create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --name "arkansas-${RESOURCE_SUFFIX}-plan" --location "${LOCATION}" --sku B1 --is-linux
 
 az webapp create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --plan "arkansas-${RESOURCE_SUFFIX}-plan" --name "arkansas-${RESOURCE_SUFFIX}-app" --runtime "DOTNETCORE:7.0"
+
+az monitor app-insights component create --app "arkansas-${RESOURCE_SUFFIX}-app" --location "${LOCATION}" --kind "web" --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --workspace "/subscriptions/${SUBSCRIPTION_ID}/resourcegroups/arkansas-${RESOURCE_SUFFIX}-rg/providers/microsoft.operationalinsights/workspaces/arkansas-${RESOURCE_SUFFIX}-workspace"
