892552af16f9f2466b4b839ca460d37718bab7d1
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:12:00 +0000

    creating app service plan

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ LOCATION="westus"
 SUBSCRIPTION_ID=$(az account show --query "id" --output "tsv")
 
 az group create --name "arkansas-${RESOURCE_SUFFIX}-rg" --location "${LOCATION}"
+
+az appservice plan create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --name "arkansas-${RESOURCE_SUFFIX}-plan" --location "${LOCATION}" --sku B1 --is-linux
