d1c553d5a7df2aae686ffd2af5197cc13a55c60d
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:25:13 +0000

    getting connection string for the application insights resource

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,5 @@ az webapp create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --plan "arkan
 az monitor log-analytics workspace create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --location "${LOCATION}" --sku "PerGB2018" --workspace-name "arkansas-${RESOURCE_SUFFIX}-workspace" --output "none"
 
 az monitor app-insights component create --app "arkansas-${RESOURCE_SUFFIX}-app" --location "${LOCATION}" --kind "web" --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --workspace "/subscriptions/${SUBSCRIPTION_ID}/resourcegroups/arkansas-${RESOURCE_SUFFIX}-rg/providers/microsoft.operationalinsights/workspaces/arkansas-${RESOURCE_SUFFIX}-workspace" --output "none"
+
+APPLICATION_INSIGHTS_CONNECTION_STRING=$(az monitor app-insights component show --app "arkansas-${RESOURCE_SUFFIX}-app" --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --query "connectionString" --output "tsv")
