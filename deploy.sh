5de76cada7d8a1ad68601517963e7a5738cbd3fe
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:12:36 +0000

    ensuring application-insights extension installed for azure cli

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+az extension add -n application-insights
+
 RESOURCE_SUFFIX=$(openssl rand -base64 20 | tr -dc 'a-z0-9' | head -c 6)
 LOCATION="westus"
 SUBSCRIPTION_ID=$(az account show --query "id" --output "tsv")
