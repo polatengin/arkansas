60b39804397f47198a9c43896393237f5bfdef95
Engin Polat <polatengin@hotmail.com>
2023-03-01 00:12:09 +0000

    creating web app

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ SUBSCRIPTION_ID=$(az account show --query "id" --output "tsv")
 az group create --name "arkansas-${RESOURCE_SUFFIX}-rg" --location "${LOCATION}"
 
 az appservice plan create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --name "arkansas-${RESOURCE_SUFFIX}-plan" --location "${LOCATION}" --sku B1 --is-linux
+
+az webapp create --resource-group "arkansas-${RESOURCE_SUFFIX}-rg" --plan "arkansas-${RESOURCE_SUFFIX}-plan" --name "arkansas-${RESOURCE_SUFFIX}-app" --runtime "DOTNETCORE:7.0"
