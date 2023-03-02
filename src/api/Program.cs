d11f3025339a7715ac5f7d82ef20b0052295bce9
Engin Polat <polatengin@hotmail.com>
2023-03-02 04:57:42 +0000

    adding home and doctors endpoints

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", new HomeHandler().Execute());
+app.MapGet("/doctors", new DoctorsHandler().Execute());
 
 app.Run();
