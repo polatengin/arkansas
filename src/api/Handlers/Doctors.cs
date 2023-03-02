5f6614ba4126509ad8dd9efdfba5b1f1110677c0
Engin Polat <polatengin@hotmail.com>
2023-03-02 04:57:23 +0000

    adding telemetry to the doctors handler

--- src/api/Handlers/Doctors.cs
+++ src/api/Handlers/Doctors.cs
@@ -3,12 +3,16 @@ public class DoctorsHandler: IHandler
   public RequestDelegate Execute()
   {
     return (context) => {
+      TelemetryHelper.TrackRequest(context);
+
       var doctors = new List<Doctor>
       {
         new Doctor { Id = 1, Name = "Dr. House" },
         new Doctor { Id = 2, Name = "Dr. Strange" },
         new Doctor { Id = 3, Name = "Dr. Manhattan" },
       };
+
+      TelemetryHelper.TrackEvent(context, $"DoctorsHandler.Execute.{doctors.Count}");
 
       return context.Response.WriteAsJsonAsync(doctors);
     };
