4fc63b2277545e6145b51ca65a84bf5da04e7692
Engin Polat <polatengin@hotmail.com>
2023-03-02 04:56:54 +0000

    adding telemetry to the home handler

--- src/api/Handlers/Home.cs
+++ src/api/Handlers/Home.cs
@@ -3,6 +3,10 @@ public class HomeHandler: IHandler
   public RequestDelegate Execute()
   {
     return (context) => {
+      TelemetryHelper.TrackRequest(context);
+
+      TelemetryHelper.TrackEvent(context, "HomeHandler.Execute");
+
       return context.Response.WriteAsJsonAsync("Hello World!");
     };
   }
